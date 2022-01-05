7e756930df3c00ae486cea9b4034c5900bf6fdc8
Rama Jawara <1811102441081@umkt.ac.id>
2022-01-06 02:23:24 +0800

    second push

--- Backup/myproject/biodata/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . models import Biodata
-
-class BiodataAdmin(admin.ModelAdmin):
-    list_display = ('user', 'nama','telp','alamat')
-
-admin.site.register(Biodata, BiodataAdmin)
--- Backup/myproject/biodata/form.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm 
-from .models import Biodata
-
-class CustomerForm(ModelForm):
-    class Meta:
-        model = Biodata
-        fields = '__all__'
